7d62c498981fcf92913bfd26e3329132e3ea92cb
Derek Beck <derekbeck@icloud.com>
2016-06-17 13:41:10 +0100

    Adds some tests to the app

--- src/test/java/test/Login/AppTest.java
+++ src/test/java/test/Login/AppTest.java
@@ -1,38 +1,42 @@
 package test.Login;
 
-import junit.framework.Test;
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import login.Decryption;
+import login.Strings;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	// Tests if the decryption class can decrypt a given string
+	@Test
+	public void testDecryption() {
+		String input = "OAY92/NZ1najV9H8iJ5OZgpJkZWQsygi";
+		String actual = new Decryption().doDecrypt(input);
+		assertEquals("Teddy Bear", actual);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	// Tests if decrypting a different string will result in a not expected username
+	@Test
+	public void testDecryptionWithIncorrectData() {
+		String input = "fg8INiEeTvVoo1lSnETqpfmZow+3fcGp";
+		String actual = new Decryption().doDecrypt(input);
+		assertNotSame("admin", actual);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	// Tests if Strings class returns the correct username
+	@Test
+	public void testStringsReturnsCorrectUsername() throws IOException {
+		String s = new Strings().getuName();
+		assertEquals("admin", s);
+	}
+	
+	// Tests if config.properties file exists
+	@Test
+	public void testConfigFileExists(){
+		new File("config.properties").exists();
+	}
+
 }
